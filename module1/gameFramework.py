e227923e7dc11798b87e127521706c4c497e01d1
Tsugua977 <augustpelissero@gmail.com>
2022-10-01 18:17:06 -0700

    Fixed screen problem. Added bullet shooting.

--- module1/gameFramework.py
+++ module1/gameFramework.py
@@ -1,14 +1,17 @@
 #Imports arcade library.
 import arcade
 
-#Creates a screen for the game.
 #Constants
-SCREEN_WIDTH = 600
-SCREEN_LENGTH = 800
+#Sets the screen width.
+SCREEN_WIDTH = 400
+#Sets the screen height.
+SCREEN_LENGTH = 600
+#Names the title of the screen.
 SCREEN_TITLE = "Space Shooter"
-SCALING = 2.0
-
-
+#Predetermined scaling size to make sprites larger.
+SCALING = 1.5
+#Predetermined bullet speed.
+BULLET_SPEED = 10
 
 #Classes
 class SpaceShooterGame (arcade.Window):
@@ -20,13 +23,16 @@ class SpaceShooterGame (arcade.Window):
 
         super().__init__(width, height, title)
 
+        #Set the lists to be empty.
         self.player_list = None
+        self.bullet_list = None
 
     #Sets up the game.
     def setup(self):
 
-        #Settng up the empty game Lists
+        #Settng up the arcade lists.
         self.player_list = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
 
         #Picks the color for the background of the screen.
         arcade.set_background_color(arcade.color.BLACK)
@@ -35,10 +41,15 @@ class SpaceShooterGame (arcade.Window):
         self.player = arcade.Sprite("module1/spaceship.png", SCALING)
         self.player.center_x = 50
         self.player.center_y = 50
-        self.player_list(self.player)
+        self.player_list.append(self.player)
 
     #Class tells the program what keys are being pressed.
-    def pressing_key(self, symbol, modifiers):
+    def on_key_press(self, symbol, modifiers):
+
+        #When Q is pressed, the game closes.
+        if symbol == arcade.key.Q:
+            arcade.close_window()
+
         #If the left arrow key is being pressed, the ship moves left.
         if symbol == arcade.key.LEFT:
             self.player.change_x = -5
@@ -47,8 +58,26 @@ class SpaceShooterGame (arcade.Window):
         if symbol == arcade.key.RIGHT:
             self.player.change_x = 5
 
+        #When Z is pressed, a bullet is fired from the player.
+        if symbol == arcade.key.Z:
+            #Create the bullet by finding its image.
+            bullet = arcade.Sprite("module1/bullet.png")
+
+            #Sets the bullet's y speed to the predetermined value.
+            bullet.change_y = BULLET_SPEED
+
+            #Centers the bullet at the center x coordinate of the player.
+            bullet.center_x = self.player.center_x
+
+            #Places the bottom of the bullet at the top of the player.
+            bullet.bottom = self.player.top
+
+            #PAppends newly created bullet into the list.
+            self.bullet_list.append(bullet)
+
+
     #Class tells the program what keys are being released.
-    def releasing_key(self, symbol: int, modifiers: int):
+    def on_key_release(self, symbol, modifiers):
         #If either the left or right arrow keys are not being pressed, the ship's speed goes to zero.
         if (
             symbol == arcade.key.LEFT
@@ -57,9 +86,11 @@ class SpaceShooterGame (arcade.Window):
             self.player.change_x = 0
 
     #Updates the game so the objects ingame move.
-    def on_update(self):
+    def on_update(self, delta_time):
 
+        #Updates the player and bullet lists to move them.
         self.player_list.update()
+        self.bullet_list.update()
 
         #If the player hits the left or right side of the screen,
         #this updates the player's ship to stay inside.
@@ -68,10 +99,21 @@ class SpaceShooterGame (arcade.Window):
         if self.player.left < 0:
             self.player.left = 0
 
+        #When the bottom of the bullet leaves the top of the screen, 
+        #the bullet is removed from the bullet list.
+        for bullet in self.bullet_list:
+            if bullet.bottom > SCREEN_LENGTH:
+                bullet.remove_from_sprite_lists()
+
     #Draws the game objects onto the screen.
     def on_draw(self):
+
+        #Starts the rendering process.
         arcade.start_render()
+
+        #Draws all the sprites in the lists.
         self.player_list.draw()
+        self.bullet_list.draw()
 
 #Allows for the game to be run.
 def main():
