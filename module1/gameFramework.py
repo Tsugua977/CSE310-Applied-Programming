d23e9435a0ffc727070d2d59a48091c571d218fc
Tsugua977 <augustpelissero@gmail.com>
2022-10-01 19:34:11 -0700

    Added shooting and enemies.

--- module1/gameFramework.py
+++ module1/gameFramework.py
@@ -26,6 +26,7 @@ class SpaceShooterGame (arcade.Window):
         #Set the lists to be empty.
         self.player_list = None
         self.bullet_list = None
+        self.enemy_list = None
 
     #Sets up the game.
     def setup(self):
@@ -33,21 +34,32 @@ class SpaceShooterGame (arcade.Window):
         #Settng up the arcade lists.
         self.player_list = arcade.SpriteList()
         self.bullet_list = arcade.SpriteList()
+        self.enemy_list = arcade.SpriteList()
 
         #Picks the color for the background of the screen.
         arcade.set_background_color(arcade.color.BLACK)
 
-        #Sets up the player.
+        #Sets up the player's sprite image.
         self.player = arcade.Sprite("module1/spaceship.png", SCALING)
-        self.player.center_x = 50
+        #Places player on screen with x and y coordinates.
+        self.player.center_x = 200
         self.player.center_y = 50
+        #Appends player to player list.
         self.player_list.append(self.player)
+
+        #Sets up the enemy's sprite image.
+        self.enemy = arcade.Sprite("module1/enemy.png")
+        #Places enemy on screen with x and y coordinates.
+        self.enemy.center_x = 200
+        self.enemy.center_y = 550
+        #Appends enemy to enemy list.
+        self.enemy_list.append(self.enemy)
 
     #Class tells the program what keys are being pressed.
     def on_key_press(self, symbol, modifiers):
 
         #When Q is pressed, the game closes.
-        if symbol == arcade.key.Q:
+        if symbol == arcade.key.ESCAPE:
             arcade.close_window()
 
         #If the left arrow key is being pressed, the ship moves left.
@@ -75,7 +87,6 @@ class SpaceShooterGame (arcade.Window):
             #PAppends newly created bullet into the list.
             self.bullet_list.append(bullet)
 
-
     #Class tells the program what keys are being released.
     def on_key_release(self, symbol, modifiers):
         #If either the left or right arrow keys are not being pressed, the ship's speed goes to zero.
@@ -91,6 +102,7 @@ class SpaceShooterGame (arcade.Window):
         #Updates the player and bullet lists to move them.
         self.player_list.update()
         self.bullet_list.update()
+        self.enemy_list.update()
 
         #If the player hits the left or right side of the screen,
         #this updates the player's ship to stay inside.
@@ -104,6 +116,11 @@ class SpaceShooterGame (arcade.Window):
         for bullet in self.bullet_list:
             if bullet.bottom > SCREEN_LENGTH:
                 bullet.remove_from_sprite_lists()
+            #If a bullet collides with an enemy, both the enemy
+            #and the bullet are removed from the list.
+            if bullet.collides_with_list(self.enemy_list):
+                bullet.remove_from_sprite_lists()
+                self.enemy.remove_from_sprite_lists()
 
     #Draws the game objects onto the screen.
     def on_draw(self):
@@ -114,6 +131,7 @@ class SpaceShooterGame (arcade.Window):
         #Draws all the sprites in the lists.
         self.player_list.draw()
         self.bullet_list.draw()
+        self.enemy_list.draw()
 
 #Allows for the game to be run.
 def main():
