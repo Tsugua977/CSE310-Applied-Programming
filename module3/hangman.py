ae3ca5458b2c524c98e0a4e4a16352669329e493
Tsugua977 <augustpelissero@gmail.com>
2022-10-27 12:40:43 -0700

    Finished game loop and win conditions

--- module3/hangman.py
+++ module3/hangman.py
@@ -1,4 +1,8 @@
+import random
+
 lives = 6
+wordList = ("acid", "coal", "come", "head", "shelf", "shine", "earth", "dodge", "damage", "export")
+randomWord = random.choice(wordList)
 
 def drawHangman(life):
     if life == 6:
@@ -59,7 +63,42 @@ def drawHangman(life):
         "---------")
         print("Game Over")
 
+print("Welcome to hangman!")
+
+drawHangman(lives)
+
 while lives !=0:
+    lives = lives
+    lettersGuessed = ""
+
+    print(randomWord)
+
+    userInput = input("Please select a letter: ")
+
+    if userInput in randomWord:
+        print(f"The letter {userInput} is in the more one or more times!")
+    else:
+        lives -= 1
+        print(f"The letter {userInput} is not in the word. {lives} more guesses remain.")
+
+    lettersGuessed += userInput
+    hasWon = 0
+
+    for letter in randomWord:
+        if letter in lettersGuessed:
+            print(f"{letter}", end="")
+        else:
+            print("_", end="")
+            hasWon += 1
+
+    print("\n")
+
     drawHangman(lives)
-    input("Welcome to hangman! Please select a letter.")
-    break
+
+    if hasWon == 0:
+        print("Congradulations! You guessed the right word!")
+        break
+
+    if lives == 0:
+        print(f"You have run out of guesses! The word was {randomWord}.")
+        break
