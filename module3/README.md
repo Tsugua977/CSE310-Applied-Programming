93e81c5f362a3848dd6ec92905a591e9941381f7
Tsugua977 <augustpelissero@gmail.com>
2022-10-28 13:59:05 -0700

    Finished Module 3!

---
+++ module3/README.md
@@ -0,0 +1,34 @@
+# Overview
+{Provide a description the game that you wrote. Describe how to play your game.}
+I created a hangman game in Python. First, the computer chooses a word. Then, the user inputs a letter a letter to guess the word.
+The user has 6 tries if they input the wrong letter. If they run out of lives, then the game ends. If they guess the correct word
+before they run out of lives, then the player wins.
+
+{Describe your purpose for writing this software.}
+I wrote this software to refresh my memory with Python. I have not used the programming language in depth for a year, so this program
+was built to remind myself of the basics of Python.
+
+{Provide a link to your YouTube demonstration.  It should be a 4-5 minute demo of the game being played and a walkthrough of the code.}
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+{Describe the tools that you used to develop the software}
+I used the random module to pick a random word for the player to guess.
+
+{Describe the programming language that you used and any libraries.}
+I used VSCode for programming Python.
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+* [W3Schools](https://www..com/)
+* [WDN Web Docs](https://developer.mozilla.org/en-US/)
+
+# Future Work
+
+{Make a list of things that you need to fix, improve, and add in the future.}
+* Ask if the player wants to play.
+* Don't let the player input letters they have already inputed. 
+* Don't let the player input an invaled characters.
--- module3/hangman.py
+++ module3/hangman.py
@@ -1,9 +1,19 @@
+#Imports the random module.
 import random
 
+#Sets up the lives to 6.
 lives = 6
+
+#Sets up the lettersGuessed to print out the guessed word.
+lettersGuessed = ""
+
+#Sets up the word list.
 wordList = ("acid", "coal", "come", "head", "shelf", "shine", "earth", "dodge", "damage", "export")
+
+#Picks a word randomly from the word list.
 randomWord = random.choice(wordList)
 
+#Function that draws the stages of the hangman based on the life.
 def drawHangman(life):
     if life == 6:
         print("  +-----+  \n"
@@ -63,42 +73,66 @@ def drawHangman(life):
         "---------")
         print("Game Over")
 
+#Prints the welcome statement.
 print("Welcome to hangman!")
 
+#Draws the hangman before the while loop.
 drawHangman(lives)
 
+#Draws the guessed word before the while loop.
+for letter in randomWord:
+        if letter in lettersGuessed:
+            print(f"{letter}", end="")
+        else:
+            print("_", end="")
+
+#Prints a break in the line.
+print("\n")
+
+#While loop runs while lives is not equal to 0.
 while lives !=0:
+
+    #Sets the lives to lives.
     lives = lives
-    lettersGuessed = ""
 
-    print(randomWord)
-
+    #Asks the user for a letter.
     userInput = input("Please select a letter: ")
 
+    #If the user's letter is in the word, then this statement is printed.
     if userInput in randomWord:
         print(f"The letter {userInput} is in the more one or more times!")
+    #If the user's letter is not in the word, one life is subtracted and the statement is printed.
     else:
         lives -= 1
         print(f"The letter {userInput} is not in the word. {lives} more guesses remain.")
 
+    #The user's letter is put into the lettersGuessed.
     lettersGuessed += userInput
+
+    #Sets hasWon to 0.
     hasWon = 0
 
+    #Draws the hangman state.
+    drawHangman(lives)
+
+    #If the letter is in lettersGuessed, it is printed onto the screen.
     for letter in randomWord:
         if letter in lettersGuessed:
             print(f"{letter}", end="")
         else:
             print("_", end="")
+            #If any "_" are in lettersGuessed, hasWon is set to 1.
             hasWon += 1
 
+    #Prints a break in the line.
     print("\n")
 
-    drawHangman(lives)
-
+    #If there are no more "_" in lettersGuessed, the game ends.
     if hasWon == 0:
-        print("Congradulations! You guessed the right word!")
+        print("Congratulations! You guessed the right word!")
         break
 
+    #If the lives equal 0, the game ends and the game breaks out of the loop.
     if lives == 0:
         print(f"You have run out of guesses! The word was {randomWord}.")
         break
